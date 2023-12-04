052e28a3e5404134db435bc2de8157c2b5d0f799
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2023-12-04 17:43:56 +0300

    Added solution for Concatenation of Array

--- PYt/week 1/concatenation-of-array.py
+++ PYt/week 1/concatenation-of-array.py
@@ -1,7 +1,5 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-        ans = nums.copy()
-        for i in range(len(nums)):
-            ans.append(nums[i])
+        ans = nums + nums
         return ans
+        
