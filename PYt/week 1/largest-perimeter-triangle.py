517ad46dbb516cb4774e11baeab38a084340ec87
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2023-12-06 17:57:02 +0300

    Added solution for Largest Perimeter Triangle

---
+++ PYt/week 1/largest-perimeter-triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        perimeter = 0
+        for i in range(len(nums)-2):
+            if nums[i] + nums[i+1] > nums[i+2]:
+                perimeter = max(perimeter, (sum(nums[i:i+3])))
+            
+        return perimeter
+
+        
