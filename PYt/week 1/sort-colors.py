6290517dbef8149e80594f9348fd8f57ccb322f7
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2023-12-27 19:03:36 +0300

    Added solution for Sort Colors

---
+++ PYt/week 1/sort-colors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(1, len(nums)):
+            for j in range(i, 0, -1):
+                if nums[j] < nums[j-1]:
+                    temp = nums[j]
+                    nums[j] = nums[j-1]
+                    nums[j-1] = temp
+        
