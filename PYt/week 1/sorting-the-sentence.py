11805fa89d3b4b67463e70b4703ba737a245a16a
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2023-12-29 13:38:36 +0300

    Added solution for Sorting the Sentence

--- PYt/week 1/sorting-the-sentence.py
+++ PYt/week 1/sorting-the-sentence.py
@@ -2,7 +2,6 @@ class Solution:
     def sortSentence(self, s: str) -> str:
         s_list = s.split()
         ans = [" "]*(len(s_list)+1)
-        print(ans)
         for word in s_list:
             i = int(word[-1])
             ans[i] = word[:-1]
